62ec1b1310691458b9a0e6bc7984684998777657
Luciano Ratamero <luciano@ratamero.com>
2020-12-09 02:38:26 -0300

    Add previous talks page

---
+++ src/routes/previous-talks.svelte
@@ -0,0 +1,204 @@
+<svelte:head>
+  <title>previous talks - luciano@ratamero.com</title>
+  <meta
+    name="description"
+    content="Luciano Ratamero's previous talks, with videos, slides and resources :]" />
+</svelte:head>
+
+<h1>Talks</h1>
+
+<h3><strong>2020</strong></h3>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://docs.google.com/presentation/d/1qEB52rYWRCaroXL0kHTkHm8JzKWj7qJw9-ro1nEVAgg/edit?usp=sharing">
+    Pyjamas - Django: from the request to the response
+  </a>
+  -
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://youtu.be/6FnAweNRBcg?t=10474">
+    Youtube
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://docs.google.com/presentation/d/1DNQlmX6_8TBnTyNSv4xU_2D4Q9EWYgeOKJoOMmGnoQU/edit?usp=sharing">
+    FrontJS - Svelte: componentes sem boilerplates
+  </a>
+  -
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.youtube.com/watch?v=zpIzknpnc8E">
+    Youtube
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://docs.google.com/presentation/d/1UdHQgmrTiwx729JzNucm6iw6fEQ0lHP-jBlBd9sMQpk/edit?usp=sharing">
+    Maneiras Svelte de lidar com APIs
+  </a>
+  -
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://youtu.be/hgpZNX6NmmU?t=553">
+    Youtube
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://docs.google.com/presentation/d/1l2oA0ovep25IW9z2bvkKiFcR90TomTa2s7T-H3VN8Gg/edit?usp=sharing">
+    FrontJS - rel-events: redux + APIs
+  </a>
+  -
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.youtube.com/watch?v=wvEMpH2TRzA">
+    Youtube
+  </a>
+</p>
+
+<h3><strong>2019</strong></h3>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2019/apaixonando-se por python/">
+    PUG-PE - apaixonando-se por python
+  </a>
+</p>
+
+<h3><strong>2018</strong></h3>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2018/pybr - Django - da requisição à resposta/">
+    Python Brasil - Django - da requisição à resposta
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2018/python para desenvolvedores javascript/">
+    PythOnRio - python para desenvolvedores javascript
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2018/Django - da requisição à resposta/">
+    PythOnRio - Django - da requisição à resposta
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2018/API Star: novo framework REST do criador do DRF/">
+    Python Sudeste - API Star: novo framework REST do criador do DRF
+  </a>
+</p>
+
+<h3><strong>2017</strong></h3>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2017/apaixonando-se por python/">
+    OpenDev/PythOnRio - apaixonando-se por python
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2017/apistar: novo framework REST feito para python3/">
+    PythOnRio - apistar: novo framework REST feito para python3
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2017/Django%20%2B%20React%3A%20dicas%20para%20quem%20quer%20fazer%20sua%20primeira%20app%20desacoplada/">
+    PythOnRio - Django + React: dicas para quem quer fazer sua primeira app
+    desacoplada
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2017/como usar django nos tempos de angular e react/">
+    Python Sudeste - Como usar Django nos tempos de Angular e React
+  </a>
+</p>
+
+<h3><strong>2016</strong></h3>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2016/fazendo seu site - blog no Github Pages com Python e Lektor/">
+    PythOnRio - fazendo seu site/blog no Github Pages com Python e Lektor
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2016/introdução ao domain driven design com django/">
+    PythOnRio - introdução ao Domain Driven Design com Django
+  </a>
+</p>
+
+<h3><strong>2015</strong></h3>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2015/tdc - poa - segregated dom/">
+    TDC - POA - Trilha JS - JS limpo e testável com Segregated DOM
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="https://www.ratamero.com/talks/2015/tdc - poa - django-pipeline/">
+    TDC - POA - Trilha Python - Front-end descomplicado com django-pipeline
+  </a>
+</p>
+<p>
+  <a
+    rel="noreferrer noopener"
+    target="_blank"
+    href="http://github.com/lucianoratamero/django-pipeline-example/">
+    Repositório do django-pipeline-example
+  </a>
+</p>
+
+<style>
+  h1 {
+    margin-bottom: 1rem;
+  }
+
+  h3,
+  p {
+    margin: 0.4rem 0;
+  }
+</style>
