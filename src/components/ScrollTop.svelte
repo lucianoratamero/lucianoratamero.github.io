961469b51069b3cb7eaa143217942af6c98843c5
Luciano Ratamero <luciano@ratamero.com>
2021-01-03 19:25:44 -0300

    Add scroll top navigation and meta tags to blog pages

---
+++ src/components/ScrollTop.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    setTimeout(() => {
+      document.documentElement.scrollTop = 0;
+    }, 50);
+  });
+</script>
--- src/routes/blog/page/[pageNumber].svelte
+++ src/routes/blog/page/[pageNumber].svelte
@@ -10,10 +10,22 @@
 </script>
 
 <script lang="ts">
+  import { stores } from "@sapper/app";
+  import ScrollTop from "../../../components/ScrollTop.svelte";
   import PaginatedPosts from "../../../components/PaginatedPosts.svelte";
 
   export let posts: { slug: string; title: string; html: any }[];
   export let pageRange, pageNumber;
+
+  const { page } = stores();
 </script>
 
+<svelte:head>
+  <title>Recent posts - Page {pageNumber} - luciano@ratamero.com</title>
+  <meta name="description" content="Luciano Ratamero's recent blog posts - Page {pageNumber}" />
+</svelte:head>
+
+{#key $page}
+  <ScrollTop />
+{/key}
 <PaginatedPosts {posts} {pageNumber} {pageRange} />
