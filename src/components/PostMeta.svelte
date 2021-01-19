90f5d0806fd03f5ba6b990ec723b5e072af3ab8c
Luciano Ratamero <luciano@ratamero.com>
2021-01-18 21:11:38 -0300

    Add PostMeta component for read time indicator

---
+++ src/components/PostMeta.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Icon from "svelte-awesome/components/Icon.svelte";
+  import ReadingTimer from "@untemps/svelte-readotron/src/core/ReadingTimer";
+  import { faClock } from "@fortawesome/free-regular-svg-icons";
+
+  export let date: string, text: string;
+  let readingTime;
+
+  $: if (text) {
+    readingTime = new ReadingTimer().getTime(text);
+  }
+</script>
+
+<p class="meta">
+  {date}
+  {#if readingTime}
+    -
+    <Icon data={faClock} scale={0.8} />
+    {readingTime}
+    min read
+  {/if}
+</p>
